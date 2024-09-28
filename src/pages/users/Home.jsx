2f4a0cf9ecd10073b4afc4c02d51f926a524c113
raihan-rahoof <mcrahoof28@gmail.com>
2024-09-28 10:45:24 +0530

    add genre categories

--- src/pages/users/Home.jsx
+++ src/pages/users/Home.jsx
@@ -1,30 +1,28 @@
-import React , {useContext, useEffect, useState} from 'react'
-import Navbar from '../../components/users/Navbar'
-import './Home.scss'
-import  AuthContext  from '../../Context/AuthContext';
-import { Navigate } from 'react-router-dom';
-import RowPost from '../../components/users/RowPost';
-import createAxiosInstance from '../../utlis/axiosinstance';
-import { Spinner } from '@nextui-org/react';
-
-
+import React, { useContext, useEffect, useState } from "react";
+import Navbar from "../../components/users/Navbar";
+import "./Home.scss";
+import AuthContext from "../../Context/AuthContext";
+import { Navigate } from "react-router-dom";
+import RowPost from "../../components/users/RowPost";
+import createAxiosInstance from "../../utlis/axiosinstance";
+import { Spinner } from "@nextui-org/react";
 
 function Home() {
-  const {isLoggedIn,setIsLoggedIn} = useContext(AuthContext)
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
   const [movies, setMovies] = useState([]);
-  const [isloading, setloading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
   const [backgroundImage, setBackgroundImage] = useState([]);
-  const axiosInstance = createAxiosInstance('user')
+  const axiosInstance = createAxiosInstance("user");
 
   const fetchMovies = async () => {
-    setloading(true);
+    setLoading(true);
     try {
       const res = await axiosInstance.get("/home/movies");
-      setloading(false);
+      setLoading(false);
       setMovies(res.data);
       console.log(res.data);
     } catch (error) {
-      setloading(false);
+      setLoading(false);
       console.log(error);
     }
   };
@@ -33,12 +31,20 @@ function Home() {
     fetchMovies();
   }, []);
 
+  // Extract genres and group movies by genre
+  const moviesByGenre = movies.reduce((acc, movie) => {
+    if (!acc[movie.genre]) {
+      acc[movie.genre] = []; // Initialize the genre array if it doesn't exist
+    }
+    acc[movie.genre].push(movie); // Add the movie to the corresponding genre
+    return acc;
+  }, {});
 
-  
-  
+  const genres = Object.keys(moviesByGenre); // Get the unique genres
+
   return (
     <>
-      {isloading ? (
+      {isLoading ? (
         <div className="h-screen flex justify-center items-center">
           <Spinner size="lg" label="Loading..." />
         </div>
@@ -53,11 +59,19 @@ function Home() {
             <Navbar />
             <div className="home-overlay"></div>
           </div>
-          <RowPost heading="Latest" movies={movies} isloading={isloading} />
+          {/* Render RowPost for each genre */}
+          {genres.map((genre) => (
+            <RowPost
+              key={genre}
+              heading={genre}
+              movies={moviesByGenre[genre]}
+              isloading={isLoading}
+            />
+          ))}
         </>
       )}
     </>
   );
 }
 
-export default Home
+export default Home;
